be8328ad2333c88de3d4306bdddc769b265aee5b
wuben3125 <wuben3125@gmail.com>
2020-03-08 00:12:11 -0500

    Adding preexisting week 1 files

---
+++ Week 1/Spyder Save Test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 12 16:47:10 2018
+
+@author: Ben
+"""
+
+statement = "I am going to try to save this in my Jupyter location"
+
+print(statement)
+
+# whoaaa I can now directly access this and edit it from jupyter
+
+# I'm gonna add something in below and see if, after saving it, I can run the updated code in Spyder
+
+newString = "This string contains new strings"
+
+print(newString)
+
+# hooollllyyyy moleeyyy; so yeah it works - the saved text+code might not immediately show in Spyder,
+    # but after running the file (which by the way implements the new stuff), the version of the .py file will auto-refresh in the Spyder Editor
